a48960c6ada47de80d71cafc333a913af91e67d3
mikebarrett19 <97762854+mikebarrett19@users.noreply.github.com>
2022-02-22 10:18:08 -0600

    integrated to other classes

--- src/com/GuruAirways/flights/Flights.java
+++ src/com/GuruAirways/flights/Flights.java
@@ -1,16 +1,14 @@
 package com.GuruAirways.flights;
 
-import java.util.Collection;
 import java.util.Date;
 import java.util.Map;
 
-enum Airline{};
 class Flights {
-    private Map<Integer, Airline> flights;
-    Map<Integer, Airline> getFlights(Date date){
+    Map<Integer, Airports> getFlights(Date date){
+        Map<Integer, Airports> flights = null;
         for (int i=0; i< 24; i++){ //"get request" just random airports
-            Airline curAirline = Airline.values()[(int)Math.random()*Airline.values().length];
-            flights.put(i, curAirline);
+            Airports curAirport = Airports.values()[(int)Math.random()*Airports.values().length];
+            flights.put(i, curAirport);
         }
         return flights;
     }

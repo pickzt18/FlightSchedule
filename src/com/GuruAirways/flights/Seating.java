95e02d231f98fb8bd987443cc2a56b8a7296caf9
Zachary Pick <pickzt18@gmail.com>
2022-02-22 07:54:32 -0800

    SeatMap and checkOpenSeats

--- src/com/GuruAirways/flights/Seating.java
+++ src/com/GuruAirways/flights/Seating.java
@@ -1,4 +1,26 @@
 package com.GuruAirways.flights;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Seating {
+    Map<String, Boolean> seatMap = new HashMap<>();
+
+    public Map getOpenSeats(String seat) {
+    seatMap.put("1a",true);
+    seatMap.put("1b",true);
+    seatMap.put("2a",true);
+    seatMap.put("2b",true);
+    seatMap.put("3a",true);
+    seatMap.put("3b",true);
+    seatMap.put("4a",true);
+    seatMap.put("4b",true);
+    seatMap.put("5a",true);
+    seatMap.put("5b",true);
+    return seatMap;
+    }
+
+    public Boolean checkOpenSeats(String seat){
+        return seatMap.get(seat);
+    }
 }

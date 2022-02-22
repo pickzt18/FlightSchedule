d7070983d02a98e47a8b7163d8dbc7da37edd335
Zachary Pick <pickzt18@gmail.com>
2022-02-22 11:00:34 -0800

    TicketTest

--- test/com/GuruAirways/flights/TicketTest.java
+++ test/com/GuruAirways/flights/TicketTest.java
@@ -16,4 +16,9 @@ public class TicketTest {
         System.out.println(tick);
         System.out.println(tick2);
     }
+    @Test(expected=IllegalSeatException.class)
+    public void checkTicketSeatException() throws ParseException, IllegalSeatException {
+        SimpleDateFormat formatter = new SimpleDateFormat("MM-dd-yyy");
+        Ticket tick=TicketFactory.createTicket("6a", formatter.parse("12-24-2022"), Airports.MKE, Airports.MSP);
+    }
 }
